7a6bbbe736d127705c879ad52e889edeba98f2a0
Reizkian <reizkian@vouchconcierge.com>
2023-01-24 16:12:53 +0700

    Fix path

--- admin/index.php
+++ admin/index.php
@@ -11,12 +11,12 @@
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/sign-in/">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link href="./admin/css/index.css" rel="stylesheet">
+    <link href="./css/index.css" rel="stylesheet">
 </head>
 
 <body class="text-center">
     <main class="form-signin w-100 m-auto">
-        <form method="POST" action="./admin/login.php">
+        <form method="POST" action="./login.php">
             <img class="mb-4" src="../img/logo_stt_lets.png" alt="" width="120" height="150">
 
             <div class="form-floating">
